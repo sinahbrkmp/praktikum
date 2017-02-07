dfc2aa92e603ed9962838d283ba13a83b644fee2
Sina <sina@dhcp91.oxoe.int>
2017-02-07 15:38:24 +0100

    breakout

---
+++ breakout/js/game.js
@@ -0,0 +1,78 @@
+
+var canvas = "";
+var context = "";
+
+init = function () {
+	canvas = document.getElementById("myCanvas");
+	context = canvas.getContext("2d");
+	myGameloop();
+}
+
+var ball = {
+	ballX: 100,
+	ballY: 100,
+	groesse: 15,
+	color: "Green",
+	speed: 2,
+	directionX: 1,
+	directionY: -1,
+};
+
+var paddleX = 0;
+
+paddle = function (e) {
+	var x = e.clientX;
+	var y = e.clienty;
+	paddleX = x ; 
+}
+
+draw = function () {
+	
+	context.fillStyle="PowderBlue";
+	context.fillRect(0,0,canvas.width,canvas.height);
+	context.fillStyle="red";
+	context.fillRect(paddleX-25,480,50,10);
+
+	context.fillStyle="Green"
+	context.beginPath();
+	context.arc(ball.ballX,ball.ballY,ball.groesse,0,2*Math.PI);
+	context.fill();
+
+}
+
+	
+
+myGameloop = function () {
+
+	draw();
+
+	ball.ballX += ball.directionX*ball.speed;
+	if (ball.ballX+ball.groesse>=canvas.width || ball.ballX-ball.groesse<=0) {
+		ball.directionX	*= -1;
+	}
+
+	ball.ballY += ball.directionY*ball.speed;
+	if (ball.ballY+ball.groesse>=canvas.height || ball.ballY-ball.groesse<=0) {
+		ball.directionY	*= -1;
+	}
+
+	if (ball.ballY+ball.groesse > 480&&ball.directionY==1) {
+		if (ball.ballX<=paddleX+25&&ball.ballX>=paddleX-25) {
+			ball.directionY = -1;
+			console.log("getroffen");
+			ball.speed ++;
+		} else {
+				ball.speed = 2;
+				console.log("falsch")
+		}
+		
+	}
+
+
+	
+
+	setTimeout(myGameloop,10);
+
+}
+
+ 
