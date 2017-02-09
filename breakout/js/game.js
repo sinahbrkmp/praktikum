abb148598255915e852cb7e23999701caa58b55d
Sina <sina@dhcp91.oxoe.int>
2017-02-09 12:37:49 +0100

    Sounds

--- breakout/js/game.js
+++ breakout/js/game.js
@@ -4,8 +4,9 @@ var context = "";
 var score = 0;
 var lives = 3;
 var time = 10;
+var highscore = 0;
 
-var colorBall = ["Green","DeepSkyBlue","Purple","Black","Crimson"]
+var colorBall = ["Green","DeepSkyBlue","Purple","Black","Crimson","DeepPink","DarkOrange"]
 init = function () {
 	canvas = document.getElementById("myCanvas");
 	context = canvas.getContext("2d");
@@ -43,6 +44,7 @@ draw = function () {
 
 	document.getElementById("lives").innerHTML=lives;
 	document.getElementById("score").innerHTML=score; 
+	document.getElementById("highscore").innerHTML=highscore;
 }
 
 moveBall = function () {
@@ -50,36 +52,54 @@ moveBall = function () {
 	ball.ballX += ball.directionX*ball.speed;
 	if ( ball.ballX-ball.groesse<=0) {
 		ball.directionX	= 1;
+		new Audio ("sound/wand_getroffen.wav").play();
 	}
 
 	if (ball.ballX+ball.groesse>=canvas.width ) {
 		ball.directionX	= -1;
+		new Audio ("sound/wand_getroffen.wav").play();
 	}
 
 	ball.ballY += ball.directionY*ball.speed;
 	if ( ball.ballY-ball.groesse<=0) {
 		ball.directionY	= 1;
+		new Audio ("sound/wand_getroffen.wav").play();
 	}
 
 	if (ball.ballY+ball.groesse>=canvas.height && ball.directionY==1) {
 		ball.directionY	= -1;
+		new Audio ("sound/unten.mp3").play();
 		lives --
 		if (lives == -1){
-			alert("Du hast keine Leben mehr! NEUSTART ")
-			score = 0;
-			lives = 3;
-			ball.speed = 2;
-			ball.color = (colorBall[0])
+			var audio = new Audio ("sound/die.wav")
+			audio.onended = function () {
+				alert("Du hast keine Leben mehr! NEUSTART ")
+				score = 0;
+				lives = 3;
+				ball.speed = 2;
+				ball.color = (colorBall[0])
+			};
+			audio.play();
+			ball.speed = 0;
+			lives = 0;
+			if (score > highscore){
+				highscore = score;
+			}
 		}			
 	}	
 
 	if (ball.ballY+ball.groesse > 480 && ball.directionY==1) {
 		if (ball.ballX<=paddleX+25&&ball.ballX>=paddleX-25) {
 			ball.directionY = -1;
-			score += 10
+			new Audio ("sound/paddle.wav").play();
+			score += 10;
 			if ( score % 50 == 0 ){
 				ball.speed ++;
-				ball.color=colorBall[Math.floor((score/50)%5)]
+				new Audio ("sound/speed.wav").play();
+				ball.color=colorBall[Math.floor((score/50)%7)]
+			}
+			if (score-10 === highscore){
+				new Audio ("sound/new_hiscore.wav").play();
 			}
 		} 		
 	}
